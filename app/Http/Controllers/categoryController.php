3a237f092e61de73bfc1fc97053ea08be7d85088
Ziad <ziadaiuory@gmail.com>
2019-12-14 14:02:35 +0200

    add category feature

---
+++ app/Http/Controllers/categoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\category;
+use Illuminate\Http\Request;
+
+class categoryController extends Controller
+{
+    public function create()
+    {
+        return view("categories.create");
+    }
+
+    public function store()
+    {
+        $category = new category;
+        $category->name = request("name");
+        $category->save();
+    }
+
+}
